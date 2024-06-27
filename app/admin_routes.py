3b2dd641927a3e063051c24a195cbaa9699526ae
Hein Yar Zar <yarzar9241@gmail.com>
2024-06-27 18:14:21 +0800

    admin has its own separate dashboard form user

--- app/admin_routes.py
+++ app/admin_routes.py
@@ -1,117 +1,70 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request
-from flask_login import login_required, current_user
-from app import db, bcrypt
-from app.models import User, Recipe, Post
-from app.forms import RegistrationForm, ResetPasswordForm, PostForm
+from flask import Blueprint, render_template, url_for, flash, redirect, request
+from flask_login import current_user, login_required
+from app import db, bcrypt  # Import bcrypt here
+from app.forms import AdminResetPasswordForm
+from app.models import User
 
 admin = Blueprint('admin', __name__)
 
-@admin.route('/admin/dashboard')
+@admin.route('/dashboard')
 @login_required
 def dashboard():
     if current_user.role != 'admin':
-        flash('Access denied', 'danger')
+        flash('You do not have permission to view this page.', 'danger')
         return redirect(url_for('user.index'))
     return render_template('admin_dashboard.html')
 
-@admin.route('/admin/manage_users')
+@admin.route('/manage_users')
 @login_required
 def manage_users():
     if current_user.role != 'admin':
-        flash('Access denied', 'danger')
+        flash('You do not have permission to view this page.', 'danger')
         return redirect(url_for('user.index'))
     users = User.query.all()
     return render_template('admin_manage_users.html', users=users)
 
-@admin.route('/admin/manage_recipes')
+@admin.route('/manage_recipes')
 @login_required
 def manage_recipes():
     if current_user.role != 'admin':
-        flash('Access denied', 'danger')
+        flash('You do not have permission to view this page.', 'danger')
         return redirect(url_for('user.index'))
-    recipes = Recipe.query.all()
-    return render_template('admin_manage_recipes.html', recipes=recipes)
+    # Add logic to manage recipes
+    return render_template('admin_manage_recipes.html')
 
-@admin.route('/admin/manage_posts')
-@login_required
-def manage_posts():
-    if current_user.role != 'admin':
-        flash('Access denied', 'danger')
-        return redirect(url_for('user.index'))
-    posts = Post.query.all()
-    return render_template('admin_manage_posts.html', posts=posts)
-
-@admin.route('/admin/settings')
+@admin.route('/settings')
 @login_required
 def settings():
     if current_user.role != 'admin':
-        flash('Access denied', 'danger')
+        flash('You do not have permission to view this page.', 'danger')
         return redirect(url_for('user.index'))
     return render_template('admin_settings.html')
 
-@admin.route('/admin/reset_password/<int:user_id>', methods=['GET', 'POST'])
+@admin.route('/reset_password/<int:user_id>', methods=['GET', 'POST'])
 @login_required
 def admin_reset_password(user_id):
     if current_user.role != 'admin':
-        flash('Access denied', 'danger')
+        flash('You do not have permission to view this page.', 'danger')
         return redirect(url_for('user.index'))
     user = User.query.get_or_404(user_id)
-    form = ResetPasswordForm()
+    form = AdminResetPasswordForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user.password = hashed_password
         db.session.commit()
-        flash('Password has been updated!', 'success')
+        flash(f'Password has been reset for user {user.username}.', 'success')
         return redirect(url_for('admin.manage_users'))
     return render_template('admin_reset_password.html', form=form, user=user)
 
-@admin.route('/admin/delete_user/<int:user_id>', methods=['POST'])
+@admin.route('/delete_user/<int:user_id>', methods=['POST'])
 @login_required
 def admin_delete_user(user_id):
     if current_user.role != 'admin':
-        flash('Access denied', 'danger')
+        flash('You do not have permission to perform this action.', 'danger')
         return redirect(url_for('user.index'))
     user = User.query.get_or_404(user_id)
-    db.session.delete(user)
-    db.session.commit()
-    flash('User has been deleted!', 'success')
-    return redirect(url_for('admin.manage_users'))
-
-@admin.route('/admin/delete_recipe/<int:recipe_id>', methods=['POST'])
-@login_required
-def admin_delete_recipe(recipe_id):
-    if current_user.role != 'admin':
-        flash('Access denied', 'danger')
-        return redirect(url_for('user.index'))
-    recipe = Recipe.query.get_or_404(recipe_id)
-    db.session.delete(recipe)
-    db.session.commit()
-    flash('Recipe has been deleted!', 'success')
-    return redirect(url_for('admin.manage_recipes'))
-
-@admin.route('/admin/delete_post/<int:post_id>', methods=['POST'])
-@login_required
-def admin_delete_post(post_id):
-    if current_user.role != 'admin':
-        flash('Access denied', 'danger')
-        return redirect(url_for('user.index'))
-    post = Post.query.get_or_404(post_id)
-    db.session.delete(post)
-    db.session.commit()
-    flash('Post has been deleted!', 'success')
-    return redirect(url_for('admin.manage_posts'))
-
-@admin.route('/admin/add_post', methods=['GET', 'POST'])
-@login_required
-def admin_add_post():
-    if current_user.role != 'admin':
-        flash('Access denied', 'danger')
-        return redirect(url_for('user.index'))
-    form = PostForm()
-    if form.validate_on_submit():
-        post = Post(title=form.title.data, content=form.content.data, author=current_user)
-        db.session.add(post)
+    if user:
+        db.session.delete(user)
         db.session.commit()
-        flash('Post has been created!', 'success')
-        return redirect(url_for('admin.manage_posts'))
-    return render_template('admin_add_post.html', form=form)
+        flash(f'User {user.username} has been deleted.', 'success')
+    return redirect(url_for('admin.manage_users'))
--- app/user_routes.py
+++ app/user_routes.py
@@ -1,29 +1,14 @@
 from flask import Blueprint, render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
 from app import db, bcrypt
-from app.models import User
 from app.forms import RegistrationForm, LoginForm
+from app.models import User
 
 user = Blueprint('user', __name__)
 
 @user.route('/')
-@user.route('/index')
 def index():
     return render_template('index.html')
-
-@user.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('user.index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password, role=form.role.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('user.login'))
-    return render_template('register.html', title='Register', form=form)
 
 @user.route('/login', methods=['GET', 'POST'])
 def login():
@@ -35,17 +20,80 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
+            if user.role == 'admin':
+                return redirect(url_for('admin.dashboard'))
             return redirect(next_page) if next_page else redirect(url_for('user.index'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+    return render_template('login.html', form=form)
+
+@user.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('user.index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password, role=form.role.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in.', 'success')
+        return redirect(url_for('user.login'))
+    return render_template('register.html', form=form)
 
 @user.route('/logout')
 def logout():
     logout_user()
+    flash('You have been logged out.', 'info')
     return redirect(url_for('user.index'))
 
-@user.route('/account')
+# Placeholder routes for future features
+@user.route('/add_recipe')
 @login_required
-def account():
-    return render_template('account.html', title='Account')
+def add_recipe():
+    return render_template('placeholder.html', feature="Add Recipes")
+
+@user.route('/edit_recipe')
+@login_required
+def edit_recipe():
+    return render_template('placeholder.html', feature="Edit Recipes")
+
+@user.route('/delete_recipe')
+@login_required
+def delete_recipe():
+    return render_template('placeholder.html', feature="Delete Recipes")
+
+@user.route('/connect_friends')
+@login_required
+def connect_friends():
+    return render_template('placeholder.html', feature="Connect with Friends")
+
+@user.route('/share_recipes')
+@login_required
+def share_recipes():
+    return render_template('placeholder.html', feature="Share Recipes")
+
+@user.route('/comment_recipes')
+@login_required
+def comment_recipes():
+    return render_template('placeholder.html', feature="Comment on Recipes")
+
+@user.route('/earn_achievements')
+@login_required
+def earn_achievements():
+    return render_template('placeholder.html', feature="Earn Achievements")
+
+@user.route('/create_shopping_list')
+@login_required
+def create_shopping_list():
+    return render_template('placeholder.html', feature="Create Shopping List")
+
+@user.route('/add_to_shopping_list')
+@login_required
+def add_to_shopping_list():
+    return render_template('placeholder.html', feature="Add to Shopping List")
+
+@user.route('/export_shopping_list')
+@login_required
+def export_shopping_list():
+    return render_template('placeholder.html', feature="Export Shopping List")
